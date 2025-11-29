ce2caf25954759f573a35191e119a5d6fe848874
tanyaasharma02 <tanyaasharma@google.com>
2025-11-30 02:14:33 +0530

    updating container for 2.0 version

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from the 1.0 container version!"
+    return "Hello from the 2.0 container version!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
