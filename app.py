e17387311a75f2973074474084bc2821479d688b
kdkateja <deep@qaitech.ai>
2025-12-10 07:36:38 +0530

    Update greeting message to version 1.0.1

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from the 1.0.0 container version!"
+    return "Hello from the 1.0.1 container version!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
